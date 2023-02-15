5637383c6f91774a041e18be1e40eca2ad91082c
Aaron Kwok <aaronkwok101@gmail.com>
2023-02-14 16:27:23 -0800

    test yelp api

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,20 @@
 import fetch from 'node-fetch'
 
-export const handler = async () => {
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+export const handler = async (event, context) => { // event: custom headers,,,, context: info about context the function was called in
+    console.log({ event }) // wrapped in {} to see object name when it returns
+    // console.log({ context })
+    const eventBody = JSON.parse(event.body)
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
 
     const response = await fetch(POKE_API)
     const data = await response.json()
 
+    console.log('poke handler ran')
+
     return {
         statusCode: 200, 
-        body: JSON.stringify({ data })
+        body: JSON.stringify({ 
+             pokemon: data.pokemon_entries[0]
+        })
     }
 }
---
+++ netlify/functions/yelp.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+
+export const handler = async () => {
+
+    console.log('yelp handler ran')
+
+    const location = 95008
+    const category = 'restaurants'
+
+    const url = `https://api.yelp.com/v3/businesses/search?location=${location}&term=${category}&radius=2000&sort_by=best_match&limit=5`
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.YELP_API_KEY}`
+        }
+    };
+
+    const response = await fetch(url, options);
+
+    //console.log('response', response)
+
+    const data = await response.json()
+    console.log(data)
+
+    return {
+        statusCode: 200, 
+        body: JSON.stringify( data.businesses[0] ) //function response must be a string per netlify
+    }
+}
+
+
+// fetch('https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
